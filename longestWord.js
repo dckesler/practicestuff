ebbc6a8ee0e69d96bc0ea5f137424de59bdaf968
Daniel Kesler <danielckesler@gmail.com>
2014-11-07 10:19:54 -0700

    Here's this stuff

--- longestWord.js
+++ longestWord.js
@@ -1,20 +1,19 @@
 var longest = function(str){
 	var newArr = str.split(' ');
-	var longest = [];
+	var longWord = [];
 	for(var i =0; i<newArr.length; i++){
-		debugger;
-		if(!longest){
-			longest.push(newArr[i]);
+		if(longWord.length === 0){
+			longWord.push(newArr[i]);
 		}
-		else if (newArr[i].length > longest[0].length){
-			longest = [newArr[i]];
+		else if (newArr[i].length > longWord[0].length){
+			longWord = [newArr[i]];
 		}
-		else if (newArr[i].toLowerCase()===longest[0].toLowerCase()){
+		else if (newArr[i].toLowerCase()===longWord[0].toLowerCase()){
 			continue;
 		}
-		else if (newArr[i] === longest[0].length){
-			longest.push(newArr[i]);
+		else if (newArr[i].length === longWord[0].length){
+			longWord.push(newArr[i]);
 		}
 	}
-	return longest;
+	return longWord;
 };
